3fd6adf0d78a7ff366abab73d0bf912d98454158
Erlan <vipoffacc@gmail.com>
2024-02-27 23:13:51 +0600

    first

---
+++ main.js
@@ -0,0 +1,41 @@
+// 1)
+
+// let age 
+
+// if (90 >= age && age >= 14){
+//     console.log("да");
+// } else{
+//     console.log("нет");
+// }
+
+// 2)
+
+// let a = ""; 
+// let b = 7;
+// let c = 1; 
+// let d = 2;
+// alert(a || b || c ||d) 
+
+
+// 3)
+
+// let age 
+// let hasLicense
+
+// if (age>18 && hasLicense) {
+//     console.log(true);    
+// }else{
+//     console.log(false);
+// }
+
+
+// 4)
+
+let age = 31
+
+if (age<18 || age>65) {
+    console.log('Вы либо слишком молоды, либо вам  нужно отдыхать и не писать код');
+}else{
+    console.log('Ваш возраст подходит для выполнения этой задачи');
+}
+
